23c9aab6909340d056a9cb8ab946f2d0ad00e228
Matheus A <ms.alvexx@gmail.com>
2022-08-06 16:32:24 -0300

    feat: ensure user has features that is on a role

--- tests/domain/role.spec.ts
+++ tests/domain/role.spec.ts
@@ -15,6 +15,11 @@ export class User {
   hasRole (role: Role): boolean {
     return this.roles.has(role)
   }
+
+  hasFeature (feature: Feature): boolean {
+    const features = Array.from(this.roles).flatMap(x => x.features)
+    return features.includes(feature)
+  }
 }
 
 type Feature = string
@@ -33,10 +38,37 @@ describe('When add role to user', () => {
   test('Should add role if user not have role yet', () => {
     const user = new User('user', 'user@mail.com', 'teste123', '123')
     const role = new Role('Author')
-    role.addFeature('WriteArticle')
 
     user.addRole(role)
 
     expect(user.hasRole(role)).toBeTruthy()
+  })
+
+  test('User should has feature if user has role that contains feature', () => {
+    const user = new User('user', 'user@mail.com', 'teste123', '123')
+    const role1 = new Role('Author')
+    const role2 = new Role('Maintainer')
+    const feature = 'WriteArticle'
+    role1.addFeature(feature)
+    role2.addFeature(feature)
+
+    user.addRole(role1)
+    user.addRole(role2)
+
+    expect(user.hasFeature(feature)).toBeTruthy()
+  })
+
+  test('User should not has feature if user not has any role that contains feature', () => {
+    const user = new User('user', 'user@mail.com', 'teste123', '123')
+    const role1 = new Role('Author')
+    const role2 = new Role('Maintainer')
+    const feature = 'WriteArticle'
+    role1.addFeature(feature)
+    role2.addFeature(feature)
+
+    user.addRole(role1)
+    user.addRole(role2)
+
+    expect(user.hasFeature('ReadArticle')).toBeFalsy()
   })
 })
