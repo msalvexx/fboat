9bfc9242452fa6e44bf87cb5dbbd9fb57a7363f6
Matheus Alves <ms.alvexx@outlook.com>
2022-08-06 22:34:20 -0300

    refactor: move role tests to other file

--- tests/iam.spec.ts
+++ tests/iam.spec.ts
@@ -77,21 +77,3 @@ describe('When add role to user', () => {
     expect(sut.hasPermission('DeleteArticle')).toBeTruthy()
   })
 })
-
-describe('When add permission to role', () => {
-  test('Should add one permission to role', () => {
-    const role = new Role('Maintainer')
-
-    role.addPermission('WriteArticle')
-
-    expect(role.hasPermission('WriteArticle')).toBeTruthy()
-  })
-
-  test('Should add multiple permissions to role', () => {
-    const role = new Role('Maintainer')
-
-    role.addPermissions(['WriteArticle', 'DeleteArticle'])
-
-    expect(role.hasPermission('WriteArticle')).toBeTruthy()
-  })
-})
---
+++ tests/role.spec.ts
@@ -0,0 +1,19 @@
+import { Role } from '@/iam/domain'
+
+describe('When add permission to role', () => {
+  test('Should add one permission to role', () => {
+    const role = new Role('Maintainer')
+
+    role.addPermission('WriteArticle')
+
+    expect(role.hasPermission('WriteArticle')).toBeTruthy()
+  })
+
+  test('Should add multiple permissions to role', () => {
+    const role = new Role('Maintainer')
+
+    role.addPermissions(['WriteArticle', 'DeleteArticle'])
+
+    expect(role.hasPermission('WriteArticle')).toBeTruthy()
+  })
+})
