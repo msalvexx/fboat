ee52ae7364e5b38b113efa4a6607fe94528dda9a
Matheus Alves <ms.alvexx@outlook.com>
2022-08-08 11:02:02 -0300

    feat: add verify on jwt adapter

--- src/iam/infra/adapters/jwt.ts
+++ src/iam/infra/adapters/jwt.ts
@@ -1,11 +1,15 @@
-import { TokenGenerator } from '@/iam/domain/protocols'
+import { Cryptography, TokenGenerator, TokenVerifier } from '@/iam/domain/protocols'
 
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements Cryptography {
   constructor (private readonly secret: string) {}
 
   async generate (params: TokenGenerator.Params): Promise<string> {
     return jwt.sign(params, this.secret)
+  }
+
+  async verify (params: string): Promise<TokenVerifier.Result> {
+    return jwt.verify(params, this.secret) as any
   }
 }
