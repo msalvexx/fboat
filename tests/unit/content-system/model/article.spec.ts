52176612e8a67508676fd704828302d70032726f
Matheus Alves <ms.alvexx@outlook.com>
2022-08-19 12:22:19 -0300

    feat: ensure can change article correctly

--- tests/unit/content-system/model/article.spec.ts
+++ tests/unit/content-system/model/article.spec.ts
@@ -28,6 +28,7 @@ export namespace Article {
     content: string
     isPublished?: boolean
     coverPhoto?: string
+    defaultCoverPhoto: string
     creationDate?: Date
     publishDate?: Date
     revisionDate?: Date
@@ -36,35 +37,86 @@ export namespace Article {
 
 class Article {
   readonly articleId: string
-  readonly author: Author
-  readonly title: string
-  readonly summary: string
-  readonly content: string
-  readonly isPublished: boolean
+  private _author: Author
+  private _title: string
+  private _summary: string
+  private _content: string
+  private _isPublished: boolean
+  private _coverPhoto?: string
+  private readonly defaultCoverPhoto: string
+  private _revisionDate: Date
   readonly creationDate: Date
-  readonly revisionDate: Date
-  readonly coverPhoto?: string
-  readonly publishDate?: Date
+  private _publishDate?: Date
 
   constructor (params: Article.Params) {
-    const { title, author, summary, content, isPublished, coverPhoto, creationDate, publishDate, revisionDate } = params
+    const { title, author, summary, content, isPublished, coverPhoto, creationDate, publishDate, revisionDate, defaultCoverPhoto } = params
     const now = new Date()
     now.setMilliseconds(0)
-    this.title = title
-    this.author = new Author(author)
-    this.summary = summary
-    this.content = content
-    this.isPublished = isPublished ?? false
+    this._title = title
+    this._author = new Author(author)
+    this._summary = summary
+    this._content = content
+    this._isPublished = isPublished ?? false
     this.creationDate = creationDate ?? now
-    this.revisionDate = revisionDate ?? now
-    this.publishDate = publishDate
-    this.coverPhoto = coverPhoto
+    this._revisionDate = revisionDate ?? now
+    this._publishDate = publishDate
+    this._coverPhoto = coverPhoto
+    this.defaultCoverPhoto = defaultCoverPhoto
+  }
+
+  changeArticle (params: Partial<Article.Params>): void {
+    const { title, author, summary, content, isPublished, coverPhoto } = params
+    const now = new Date()
+    now.setMilliseconds(0)
+    this._title = title ?? this._title
+    this._author = author !== undefined ? new Author(author) : this._author
+    this._summary = summary ?? this._summary
+    this._content = content ?? this._content
+    this._coverPhoto = coverPhoto ?? this._coverPhoto
+    this._revisionDate = now
+    if (isPublished === undefined) return
+    this._isPublished = isPublished ?? this._isPublished
+    if (this._isPublished) this._publishDate = now
+  }
+
+  get title (): string {
+    return this._title
+  }
+
+  get author (): Author {
+    return this._author
+  }
+
+  get summary (): string {
+    return this._summary
+  }
+
+  get content (): string {
+    return this._content
+  }
+
+  get isPublished (): boolean {
+    return this._isPublished
+  }
+
+  get coverPhoto (): string {
+    return this._coverPhoto ?? this.defaultCoverPhoto
+  }
+
+  get publishDate (): Date | undefined {
+    return this._publishDate
+  }
+
+  get revisionDate (): Date | undefined {
+    return this._revisionDate
   }
 }
 
 describe('Article', () => {
-  test('Should create article in unpublished state', () => {
-    const sut = new Article({
+  let sut: Article
+
+  beforeEach(() => {
+    sut = new Article({
       articleId: '123',
       title: 'any title',
       author: {
@@ -73,9 +125,29 @@ describe('Article', () => {
         occupation: 'any occupation'
       },
       content: '<html></html>',
-      summary: 'any summary'
+      summary: 'any summary',
+      coverPhoto: 'any',
+      defaultCoverPhoto: 'defaultPhoto'
+    })
+  })
+
+  test('Should create article in unpublished state', () => {
+    expect(sut.isPublished).toBeFalsy()
+  })
+
+  test('Should change article', () => {
+    const oldDate = sut.revisionDate
+
+    sut.changeArticle({
+      content: 'other content',
+      isPublished: true,
+      coverPhoto: 'any photo'
     })
 
-    expect(sut.isPublished).toBeFalsy()
+    expect(sut.isPublished).toBeTruthy()
+    expect(sut.revisionDate).not.toBe(oldDate)
+    expect(sut.publishDate).toBe(sut.revisionDate)
+    expect(sut.coverPhoto).toBe('any photo')
+    expect(sut.content).toBe('other content')
   })
 })
