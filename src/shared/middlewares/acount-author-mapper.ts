dc57c02a0bd96407e7451aa5c82034cb06c966a9
Matheus Alves <ms.alvexx@outlook.com>
2022-08-23 10:39:37 -0300

    feat: ensure will map logged account to author

---
+++ src/shared/middlewares/acount-author-mapper.ts
@@ -0,0 +1,15 @@
+import { Author } from '@/content-system'
+import { Account } from '@/iam'
+import { AbstractHandler } from "."
+
+export class AccountToAuthorMapperHandler extends AbstractHandler {
+  override async handle (params: any): Promise<any> {
+    if ('loggedAccount' in params) {
+      const { personalData, accountId }: Account = params.loggedAccount
+      const { fullName: name, occupation, photo } = personalData
+      const author: Author.Params = { accountId, name, occupation, photo }
+      params = { ...params, author }
+    }
+    return await super.handle(params)
+  }
+}
---
+++ tests/unit/shared/middlewares/account-to-author-mapper.spec.ts
@@ -0,0 +1,31 @@
+import { mockAccount } from '@/tests/mocks/iam'
+import { HandlerSpy } from '@/tests/mocks/shared/middlewares'
+import { AccountToAuthorMapperHandler } from '@/shared/middlewares/acount-author-mapper'
+
+describe('Account to Author Mapper Handler', () => {
+  let sut: AccountToAuthorMapperHandler
+  let spy: HandlerSpy
+
+  beforeEach(() => {
+    sut = new AccountToAuthorMapperHandler()
+    spy = new HandlerSpy()
+    sut.setNext(spy)
+  })
+
+  test('Should call super handler change loggedAccount to author param', async () => {
+    const loggedAccount = mockAccount({ user: { roles: ['Administrator'] } })
+    const params = { loggedAccount }
+
+    await sut.handle(params)
+
+    expect(spy.params).toStrictEqual({
+      ...params,
+      author: {
+        accountId: loggedAccount.accountId,
+        name: loggedAccount.personalData.fullName,
+        occupation: loggedAccount.personalData.occupation,
+        photo: loggedAccount.personalData.photo
+      }
+    })
+  })
+})
