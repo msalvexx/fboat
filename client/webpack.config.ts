dae8e33e7db404042489aff18ac43a389913018c
Matheus Alves <ms.alvexx@outlook.com>
2022-09-04 13:45:12 -0300

    chore: configure webpack

---
+++ client/webpack.config.ts
@@ -0,0 +1,39 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import 'webpack-dev-server'
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    devMiddleware: {
+      writeToDisk: true
+    },
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
+
+export default config
