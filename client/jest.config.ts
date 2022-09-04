0e9ef194592b84abec3f4167b6b1c4f59aa31104
Matheus Alves <ms.alvexx@outlook.com>
2022-09-04 11:58:00 -0300

    chore: add react configs

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -2,15 +2,15 @@ export default {
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.spec.ts'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
+    '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/index.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
