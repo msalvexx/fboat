592e52b7dc3c50d0f8c0b22a68bc99d0690ff19e
Matheus Alves <ms.alvexx@outlook.com>
2022-09-10 13:45:04 -0300

    feat: handle outside click

--- client/src/presentation/components/account-menu/index.tsx
+++ client/src/presentation/components/account-menu/index.tsx
@@ -1,19 +1,43 @@
-import React, { useState } from 'react'
+import React, { MutableRefObject, useState } from 'react'
 import Styles from './styles.scss'
 
 import { Avatar } from '@/presentation/components'
+
+const useOutsideClick = (callback: () => void): MutableRefObject<any> => {
+  const ref = React.useRef<any>()
+
+  React.useEffect(() => {
+    const handleClick = (event: Event): void => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback()
+      }
+    }
+
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  }, [ref])
+
+  return ref
+}
 
 const AccountMenu: React.FC = () => {
   const name = 'Jhon Doe'
   const email = 'jhon.doe@mail.com'
   const avatar = 'https://ui-avatars.com/api/?name=John+Doe&rounded=true'
-  const [menuState, setMenuState] = useState(true)
+  const [showMenuState, setMenuState] = useState(false)
+  const ref = useOutsideClick(() => setMenuState(false))
   return <ul>
     <li>
-      <a onClick={() => setMenuState(!menuState)}>
+      <a
+        ref={ref}
+        onClick={() => setMenuState(!showMenuState)}
+      >
         <img src={avatar} width="48" height="48"/>
       </a>
-      <div hidden={menuState} className={Styles.accountMenu}>
+      <div hidden={!showMenuState} className={Styles.accountMenu}>
           <ul>
               <Avatar title={name} subtitle={email} avatar={avatar}/>
               <li data-divider></li>
