4b8d91022097c8c3845749cc6c0ec7593bc46652
Matheus Alves <ms.alvexx@outlook.com>
2022-09-11 02:00:32 -0300

    feat: add multiselect field

---
+++ client/src/presentation/components/select/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import MultiSelect, { components, DropdownIndicatorProps, ClearIndicatorProps } from 'react-select'
+import Styles from './styles.scss'
+
+type Option = {
+  value: string
+  label: string
+}
+
+type Props = {
+  name: string
+  options: Option[]
+  defaultValue: Option[]
+  placeholder?: string
+  isMulti?: boolean
+}
+
+const DropdownIndicator: React.FC<DropdownIndicatorProps> = (props: DropdownIndicatorProps) => (
+  <components.DropdownIndicator {...props}>
+    <span data-uk-icon="chevron-down"></span>
+  </components.DropdownIndicator>
+)
+
+const ClearIndicator: React.FC<ClearIndicatorProps> = (props: ClearIndicatorProps) => (
+  <components.ClearIndicator {...props}>
+    <span data-uk-icon="close"></span>
+  </components.ClearIndicator>
+)
+
+const Select: React.FC<Props> = (props: Props) => {
+  const error = true
+  const errorMessage = 'Any error message here!'
+  const dangerColor = '#f0506e'
+  const lightDangerColor = '#ff90a5'
+  const config = {
+    borderColor: dangerColor,
+    color: dangerColor,
+    boxShadow: 'none'
+  }
+  const defaultInvalidStyle = (base: any, _): any => ({
+    ...base,
+    ...config,
+    '&:hover': config,
+    '&:focus': config
+  })
+  const multiValueInvalidStyle = (base: any, _): any => ({
+    ...base,
+    ...config,
+    '&:hover': config,
+    '&:focus': config,
+    ...{ backgroundColor: lightDangerColor }
+  })
+  const errorStyle = {
+    control: defaultInvalidStyle,
+    dropdownIndicator: defaultInvalidStyle,
+    clearIndicator: defaultInvalidStyle,
+    multiValue: multiValueInvalidStyle
+  }
+  return <>
+    <MultiSelect
+      styles={error ? errorStyle : undefined}
+      components={{ DropdownIndicator, ClearIndicator }}
+      classNamePrefix='MultiSelect'
+      className={Styles.select} {...props}/>
+    {error && <label className={Styles.errorAlert}>{errorMessage}</label>}
+  </>
+}
+
+export default Select
--- client/src/presentation/pages/my-account/components/credentials/index.tsx
+++ client/src/presentation/pages/my-account/components/credentials/index.tsx
@@ -1,14 +1,29 @@
-import { Input } from '@/presentation/components'
+import { Input, Select } from '@/presentation/components'
 import React from 'react'
 import Styles from './styles.scss'
 
 const Credentials: React.FC = () => {
+  const defaultRoles = [{
+    label: 'Escritor',
+    value: 'Writer'
+  }]
+  const roles = [{
+    label: 'Administrador',
+    value: 'Administrator'
+  }, {
+    label: 'Leitor de Informações do F-Boat',
+    value: 'FBoatReader'
+  }, {
+    label: 'Controlador do F-Boat',
+    value: 'FBoatController'
+  }, ...defaultRoles]
   return (<div className={Styles.credentials}>
       <p>Credenciais</p>
       <div data-card className="uk-width-1-1@m">
         <form className={Styles.form}>
           <Input type="email" name="email" placeholder="E-mail"/>
           <Input type="password" name="password" placeholder="Senha"/>
+          <Select name="roles" placeholder='Selecione os perfis de acesso' isMulti options={roles} defaultValue={defaultRoles}/>
         </form>
       </div>
   </div>)
